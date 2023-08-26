89e979a8173e5589ac133369f63366ffb6a93244
kfujita <kfujita@student.42tokyo.jp>
2023-08-26 15:54:58 +0900

    create Wrong Animal / Cat

---
+++ ex00/WrongCat.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/06 23:35:26 by kfujita           #+#    #+#             */
+/*   Updated: 2023/08/06 23:39:42 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal
+{
+public:
+	WrongCat();
+	WrongCat(const WrongCat& v);
+	~WrongCat();
+
+	WrongCat& operator=(const WrongCat v);
+
+	void makeSound() const;
+};
